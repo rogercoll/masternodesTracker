b1ca366e5f3dff7bec4bd928d19104a54329b265
Roger Coll <roger.coll.aumatell@gmail.com>
2020-05-13 23:36:29 +0200

    Colors viiiiiiii

--- pkg/checker/checker.go
+++ pkg/checker/checker.go
@@ -16,6 +16,14 @@ import (
 )
 
 const (
+	InfoColor    = "\033[1;34m%s\033[0m"
+	NoticeColor  = "\033[1;36m%s\033[0m"
+	WarningColor = "\033[1;33m%s\033[0m"
+	ErrorColor   = "\033[1;31m%s\033[0m"
+	DebugColor   = "\033[0;36m%s\033[0m"
+)
+
+const (
     // See http://golang.org/pkg/time/#Parse
     timeFormat = "2006-01-02 15:04 MST"
 )
@@ -78,20 +86,22 @@ func Check2() {
 	mcoins, err := db.GetCoinInfo(c, "iqcash")
 	if err != nil { log.Fatal(err)}
 	for _, master := range *mcoins {
-		fmt.Printf("Coin: %s     PublicKey: %s\n", master.Coin, master.PublicKey)
+		fmt.Printf("\033[1;33mCoin: %s     PublicKey: %s\033[0m\n", master.Coin, master.PublicKey)
 		diff := time.Now().Sub(time.Unix(master.LastCheck,0))
 		fmt.Printf("Time since last check: %2.f days %.f hours %.f minutes\n", diff.Hours()/24, diff.Hours(), diff.Minutes())
 		balance,actualHash, err := getActual(master.ApiEndpoint, master.RegexBalance)
 		if err != nil { log.Fatal(err)}
-		fmt.Printf("Actual balance: %s", balance)
+		fmt.Printf("Actual balance: %s\n", balance)
 		if actualHash !=  master.LastHash {
-			log.Printf("Masternode with public key %v has new transactions", master.PublicKey)
+			fmt.Printf("Masternode with public key %v has new transactions\n", master.PublicKey)
 			master.LastHash = actualHash
 			master.LastCheck = int64(time.Now().Unix())
 			err = db.UpdateCoinInfo(c, master.PublicKey, &master)
 			if err != nil { log.Fatal(err)}
-			log.Println("MongoDB document updated")
+			fmt.Printf(NoticeColor + "\n", "MongoDB document updated")
+		} else {
+			fmt.Printf(ErrorColor + "\n", "No changes since last check")
 		}
-		fmt.Println("		======		======		======		")
+		fmt.Println()
 	}
 }
